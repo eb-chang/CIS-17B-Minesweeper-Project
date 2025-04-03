60a39f92f05429db916275d423dd5cfc4944a80b
danny.giff <danny.giff@yahoo.com>
2025-04-03 16:18:38 -0700

    main() 1.0

--- main.cpp
+++ main.cpp
@@ -12,51 +12,33 @@ using namespace std;
 
 int main() {
 
-    /*
-        this part of main() was originally written by Brian and generates a simple minesweeper grid
-        where 0 = blank and 1 = mine
-    */
+    //set random seed
     srand(static_cast<unsigned int>(time(0)));
 
-    //config here
-   // 16x16 with 40 mines is "intermediate" difficulty
-   /*   const int rows = 16;
-    const int cols = 16;
-    int minesAmount = 40;*/
+    //declare vars
+    int r, c, m;//rows, cols, mines
 
-    /* rip brian's main fn
-    
-    int gridMines[rows][cols];
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < cols; j++){
-            gridMines[i][j] = 0;
-        }
-    }
+    //load user?
 
-    //for (int i = 0; i < minesAmount; i++) {
-    //    int r = rand() % (rows - 1);
-    //    int c = rand() % (cols - 1);
-    //    if (gridMines[r][c] == 1) i--;
-    //    else gridMines[r][c] = 1;
-    //}
+    /*
+    prompt user for difficulty
 
-    //for (int i = 0; i < rows; i++) {
-    //    for (int j = 0; j < cols; j++) {
-    //        cout << gridMines[i][j];
-    //    }
-    //    cout << endl;
-    //}
+    beginner - 8x8 (or 9x9) w/ 10 mines
+    intermediate - 16x16 w/ 40 mines
+    expert - 16x30 w/ 99 mines
+    */
+    r = 9;
+    c = 9;
+    m = 10;
 
-   /* cout << "here is an object - oriented approach using a modified version of brian's algorithm" << endl;
-    cout << endl;*/
-    //Grid board(rows, cols);
-    //board.putMines(minesAmount);
-    //board.print();
-
-    Game game(9, 9, 10); // Easy 9x9 with 10 mines
+    //create and run game
+    Game game(r, c, m); // Easy 9x9 with 10 mines
     game.run();
 
+    //display & record scoreboard
 
+    //another game?
 
+    //exit the program
     return 0;
 }
