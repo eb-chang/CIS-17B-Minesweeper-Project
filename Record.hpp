26a1e52e5055758235a32c473a5a8d89e9453089
elliec <ebc.chang@gmail.com>
2025-04-21 22:48:47 -0700

    Added useful comment

--- Record.hpp
+++ Record.hpp
@@ -2,22 +2,24 @@
 #define RECORD_H
 
 #include "User.hpp"
+#include <vector>
 
 using namespace std;
 
+// The record class contains a vector of users, and utilizers user save functions
+// to write out/read in user data for the minesweeper game. Save/load functions courtesy of Jesus
 class Record {
     private:
         int nUsers;
-        User *users;
+        vector<User> userData;
         
     public:
         Record();
-        Record(int n);
-        void saveFile(string filename);
+        Record(vector<User> data);
+        void saveFile(string filename, string name);
         void loadFile(string filename);
         void addUser(string name);
         void print();
-        virtual ~Record();
 };
 
 #endif //RECORD_H
