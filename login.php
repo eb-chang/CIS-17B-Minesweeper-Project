44727112f3130f6f4ed4a4975264d5c9277ab2b5
David Zuniga <davzun3@gmail.com>
2025-05-28 15:52:53 -0700

    Added user login

---
+++ login.php
@@ -0,0 +1,67 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Minesweeper | Account</title>
+
+    <!-- Link to your CSS -->
+    <link rel="stylesheet" href="styles.css?v=1" type="text/css" />
+
+    <!-- Load modules -->
+    <script src="Model.js"></script>
+    <script src="View.js"></script>
+    <script src="Controller.js"></script>
+    <script src="handlerInit.js"></script>
+</head>
+<body>
+    <!--navbar-->
+    <ul>
+        <li><a href="minesweeper.html">Play</a></li>
+        <li><a class="active" href="login.php">Account</a></li>
+        <li><a href="stats.html">Stats</a></li>
+        <li style="float:right"><a class="logout" href="logout.php">Logout</a></li>
+    </ul>
+
+
+    <h1>Account Management</h1>
+    <?php 
+        session_start();
+
+        //From Php manual -> Which request method was used to access the page; e.g. 'GET', 'HEAD', 'POST', 'PUT'.
+        if($_SERVER["REQUEST_METHOD"] === "POST")
+        {
+            $_SESSION["uname"] = $_POST["uname"];
+            $_SESSION["login"] = true;
+            $_SESSION["password"] = $_POST["password"];
+            $_SESSION["email"] = $_POST["email"];
+        }
+        //If user already logged in
+        if(isset($_SESSION["login"]) && $_SESSION["login"] === true)
+        {
+            echo "<p>Welcome " . $_SESSION["uname"] . "</P>";
+        }
+        //If user hasn't logged in
+        else
+        {
+            //Displays form for logging in
+            echo '<form method="post">
+                    <!-- Username Name -->
+                    <label for="uname">Username:</label><br>
+                    <input type="text" id="uname" name="uname"><br>
+            
+                    <!-- Password Name -->
+                    <label for="password">Password:</label><br>
+                    <input type="text" id="password" name="password"><br>
+            
+                    <!-- Email -->
+                    <label for="email">Email:</label><br>
+                    <input type="text" id="email" name="email"><br>
+
+                    <!-- Submit button -->
+                    <input type="submit" value="Submit">
+                </form>  ';
+        }
+    ?>
+</body>
+</html>
---
+++ logout.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+
+    <?php 
+    //Destroys session variables when user signs out
+        session_start(); 
+
+        session_unset();
+
+        session_destroy();
+        header("Location: login.php")
+    ?>
+</body>
+</html>
